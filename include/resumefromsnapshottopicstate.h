a44e4bad73acbaacc34db9b24bdf06ea643985a4
Clovis Scotti <clovisscotti@gmail.com>
2018-09-12 14:28:31 -0700

    Adding ResumeFromSnapshotTopicState for resuming graph states from a snapshot. (#3)

---
+++ include/resumefromsnapshottopicstate.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 4 indent-tabs-mode: nil -*-
+ *
+ *    Copyright (c) 2016 Nest Labs, Inc.
+ *    All rights reserved.
+ *
+ *    This document is the property of Nest. It is considered
+ *    confidential and proprietary information.
+ *
+ *    This document may not be reproduced or transmitted in any form,
+ *    in whole or in part, without the express written permission of
+ *    Nest.
+ *
+ *    Description:
+ *      TopicState that contains an entire StateSnapshot passed to
+ *    all resuming detectors on startup.
+ *      Note that this is a private TopicState (otherwise this
+ *    object would grow indefinitely mirror vs. mirror style)
+ *
+ */
+
+#ifndef RESUMEFROMSNAPSHOTTOPICSTATE_H
+#define RESUMEFROMSNAPSHOTTOPICSTATE_H
+
+#include <topicstate.hpp>
+#include "statesnapshot.hpp"
+
+struct ResumeFromSnapshotTopicState : public DetectorGraph::TopicState
+{
+    DetectorGraph::StateSnapshot snapshot;
+};
+
+#endif // RESUMEFROMSNAPSHOTTOPICSTATE_H
